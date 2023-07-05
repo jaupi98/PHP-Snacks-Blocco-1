24ada102465135ffb053d5f348db0356e8f12eac
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-07-05 15:40:19 +0200

    snack 3 completato

--- index.php
+++ index.php
@@ -50,6 +50,47 @@
             $flag_eta = true;
         }
     }
+    //     Snack 3
+    // Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
 
 ?>
 <!DOCTYPE html>
@@ -93,6 +134,25 @@
                 </h2>
             </div>
         <?php } ?>  
+    </div>
+    <hr>
+    <div>
+        <h2>Snack 3</h2>
+        <ul>
+            <?php  foreach($posts as $key => $post ){ ?>
+                <li>
+                    <?php echo $key; ?>
+                    <ul>
+                    <?php  foreach($post as $item){ ?>
+                        <li>
+                            <?php echo $item['title']."-". $item['author']."-". $item['text']; ?>
+                        </li>
+                    <?php } ?>
+                    </ul>
+                </li>
+            <?php  } ?>
+                  
+        </ul>
     </div>
     
 </body>
